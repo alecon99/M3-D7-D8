cfc39235f3674c705cc0c3c236c967db6b2ac2cc
alecon99 <123322669+alecon99@users.noreply.github.com>
2023-05-15 18:04:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+/* prova caricamento schede con lo scorrere della pagina, 
+non sono riuscito a implementarlo in modo che sia fruibile da ogni dispositivo */
+
+/*   let watchItAgain = document.getElementsByClassName("watch_it_again");
+  let newReleases = document.getElementsByClassName("new_releases");
+  
+
+window.addEventListener('scroll', function page() {
+  if (window.innerWidth < 576) {
+    watchItAgain[0].className += " show1";
+  };
+  if ((window.innerWidth > 575) && (window.innerWidth < 768)) {
+    if ( window.pageYOffset > 290) {
+      watchItAgain[0].className += " show1";
+    } 
+    if ( window.pageYOffset > 730) {
+      newReleases[0].className += " show1";
+    }
+  };
+  if ((window.innerWidth > 767) && (window.innerWidth < 992)) {
+    if ( window.pageYOffset > 100) {
+      watchItAgain[0].className += " show1";
+    } 
+    if ( window.pageYOffset > 500) {
+      newReleases[0].className += " show1";
+    }
+  };
+  if ((window.innerWidth > 991) && (window.innerWidth < 1200)) {
+    if ( window.pageYOffset > 100) {
+      watchItAgain[0].className += " show1";
+    } 
+    if ( window.pageYOffset > 570) {
+      newReleases[0].className += " show1";
+    }
+  };
+  
+}); */
+
